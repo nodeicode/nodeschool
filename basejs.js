bddcef4b81adebe76494f1354d9475268069a6cc
Lohit Ary <lgopik2@uic.edu>
2018-07-05 12:32:35 +0530

    chapter 8 half done!

---
+++ basejs.js
@@ -0,0 +1,32 @@
+/* 1:INTRODUCTION
+console.log('hello');*/
+
+/*2:VARIABLES
+var example = 'some string'
+console.log(example)*/
+
+/*3:STRINGS
+var somestring = 'this is a string'
+console.log(somestring)*/
+
+/*4: STRING LENGTH
+var example = 'example string'
+console.log(example.length)*/
+
+/*5: REVISING STRINGS:
+var pizza = 'pizza is alright'
+pizza = pizza.replace('alright','wonderful')
+console.log(pizza)*/
+
+/*6:NUMBERS
+var example = 123456789
+console.log(example)*/
+
+/*7:ROUNDING NUMBERS
+var roundUp = 1.5
+var rounded = Math.round(roundUp)
+console.log(rounded)*/
+
+//8:NUMBER TO STRING
+var n = 128
+console.log(n.toString())
