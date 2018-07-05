99b4e88e85dec77efd0980d133cc838c993d492d
Lohit Ary <lgopik2@uic.edu>
2018-07-05 12:33:14 +0530

    still completing but gonna work on python for now :?

--- node.js
+++ node.js
@@ -1,9 +1,28 @@
 /* 1:HELLO WORLD
 console.log("HELLO WORLD") */
 
-//2:BABY STEPS
- sum;
-for(i =0;i<process.argv.length();i++){
+/*2:BABY STEPS
+var sum=0;
+for(i =2;i<process.argv.length;i++){
 sum+=Number(process.argv[i]);
 };
-console.log(sum);
+console.log(sum);*/
+
+/*3: FIRST I/O!
+const fs  = require('fs')
+var str = fs.readFileSync(process.argv[2]).toString()
+str = str.split('\n')
+console.log(str.length-1)*/
+
+/*4: FIRST ASYNC I/O:
+const fs = require('fs')
+fs.readFile(process.argv[2],'utf8',(err,str)=>{if(err)console.log(Error);else console.log(str.split('\n').length-1)})*/
+
+//5: FILTERED LS
+var fs = require('fs')
+fs.readdir(process.argv[2],(err,data)=>{
+    if(err)console.log(Error)
+    else
+
+
+)})
