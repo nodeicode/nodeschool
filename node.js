27515a68f0529ddfcdce80837450833f33b316a5
Lohit Ary <lgopik2@uic.edu>
2018-07-09 19:02:54 +0530

    half done!!

--- node.js
+++ node.js
@@ -18,11 +18,29 @@ console.log(str.length-1)*/
 const fs = require('fs')
 fs.readFile(process.argv[2],'utf8',(err,str)=>{if(err)console.log(Error);else console.log(str.split('\n').length-1)})*/
 
-//5: FILTERED LS
+/*5: FILTERED LS
 var fs = require('fs')
+var path  = require('path')
 fs.readdir(process.argv[2],(err,data)=>{
     if(err)console.log(Error)
-    else
+    else{
+        for(var i=0;i<data.length;i++){
+            if(path.extname(data[i])== "."+process.argv[3]) console.log(data[i])
+        }
+    }
+})*/
 
-
-)})
+//6:MAKE IT MODULAR
+module.exports = function (dir,fil,foo){
+    var path  = require('path')
+    var fs = require('fs')
+    fs.readdir(dir,(err,data)=>{
+    if(err) return foo(err)
+    for(var i=0;i<data.length;i++){
+        if(path.extname(data[i])!= "."+fil){
+            data.splice(i,1);i--}
+}
+return foo(null,data)
+}
+)
+}
