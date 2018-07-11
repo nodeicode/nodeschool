9e62a56e2f43bbe3642dae0086ed5587ef3668d7
Lohit Ary <lgopik2@uic.edu>
2018-07-11 17:05:28 +0530

    almost Done!!!

--- node.js
+++ node.js
@@ -30,7 +30,7 @@ fs.readdir(process.argv[2],(err,data)=>{
     }
 })*/
 
-//6:MAKE IT MODULAR
+/*6:MAKE IT MODULAR
 module.exports = function (dir,fil,foo){
     var path  = require('path')
     var fs = require('fs')
@@ -39,8 +39,57 @@ module.exports = function (dir,fil,foo){
     for(var i=0;i<data.length;i++){
         if(path.extname(data[i])!= "."+fil){
             data.splice(i,1);i--}
-}
-return foo(null,data)
-}
-)
-}
+}return foo(null,data)
+})}*/
+
+/*7:HTTP CLEINT
+var http = require('http')
+http.get(process.argv[2],function foo(nso){
+    nso.setEncoding('utf8')
+    nso.on("data",(data)=>{
+        console.log(data)
+    })
+})*/
+
+/*8:HTTP COLLECT
+var http = require('http')
+http.get(process.argv[2],function foo(nso){
+    nso.setEncoding('utf8')
+    var res = ""
+    nso.on("data",(data)=>{
+        res+=data
+    })
+    nso.on("end",()=>{
+        console.log(res.length)
+        console.log(res)
+    })
+})*/
+
+/*9:JUGGLING ASYNC
+var http = require('http')
+http.get(process.argv[2],function foo(nso){
+    http.get(process.argv[3],(nso1)=>{
+        http.get(process.argv[4],(nso2)=>{
+    var res=""
+    nso.on("data",(data)=>{
+        res+=data
+    })
+
+    var res1=""
+        nso1.on("data",(data)=>{
+            res1+=data
+        })
+
+    var res2=""
+        nso2.on("data",(data)=>{
+            res2+=data
+        })
+        nso.on("end",()=>{console.log(res)
+        nso1.on("end",()=>{console.log(res1)
+        nso2.on("end",()=>{console.log(res2)
+        })})})
+})
+})
+})*/
+
+//10:TIME SERVER
